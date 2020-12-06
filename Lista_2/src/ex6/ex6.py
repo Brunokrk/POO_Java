1c06ceb19f5d1d55100c7e4e017206ab7684cade
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2020-12-06 13:44:07 -0300

    .

--- Lista_2/src/ex6/ex6.py
+++ Lista_2/src/ex6/ex6.py
@@ -10,3 +10,10 @@ class Teclado():
     
     def __str__(self):
         return f'{self.marca}, {self.padrao}, {self.cor}'
+
+teclado = Teclado("corsair", "ABNT", "PRETO")
+teclado2 = Teclado("razer", "ABNT", "BRANCO")
+
+
+print(teclado)
+print(teclado2)
