23c77d4923c02cb3a6c72a43ccceb2b2e5c187a0
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2020-12-08 10:49:08 -0300

    .

---
+++ Aula_Pratica_5.2/src/negocio/ListaTelefonica.java
@@ -0,0 +1,14 @@
+package negocio;
+
+import dados.Contato;
+import java.util.List;
+import java.util.ArrayList;
+
+public class ListaTelefonica {
+	
+	private List<Contato> contatos = new ArrayList<Contato>();
+	
+	public void adicionarContato(Contato contato) {
+		System.out.println("");
+	}
+}
